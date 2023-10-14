5ea6d1a23f3f1cbbba98e9c1d4f2c086348e0eab
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-14 18:30:34 +0100

    wrote the psuedo code for the game
    wrote the psuedo code for the game  and will begin to write the code for the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+// need a function called getComputerChoice
+// the function should return a random choice of rock, paper, or scissors
+// write a function that plays a single round of rock paper scissors
+// function should take two parameters: playerSelection and computerSelection
+// function should return a string that declares the winner of the round
+// the players selection should be case insensitive
