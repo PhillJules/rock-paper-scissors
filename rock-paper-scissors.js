4af2c1e1ab909e33d6f1795d577315f5ceb28a0d
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-14 19:21:04 +0100

    the computer randomly chooses
    the computer can now choose between rock, paper, and scissors
    i created an object to store the winning combinations
    of rock, paper, and scissors. there is now a function that
    chooses a key at random from the object. verified that by logging it to
    the console.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,23 @@
+// create an object that shows the winning combinations
+const winningCombinations = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+}
+// create a variable that stores computer choices
+const computerChoices = Object.keys(winningCombinations);
 // need a function called getComputerChoice
 // the function should return a random choice of rock, paper, or scissors
+function getComputerChoice() {
+  let randomizedChoice = Math.floor(Math.random() * computerChoices.length)
+
+  return computerChoices[randomizedChoice];
+}
+
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
 // write a function that plays a single round of rock paper scissors
 // function should take two parameters: playerSelection and computerSelection
 // function should return a string that declares the winner of the round
