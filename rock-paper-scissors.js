644323d692c2f0f165266df6632a908bc3c43350
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-14 21:29:27 +0100

    added new function called game
    the new fuction encompasses the previous functions and i added a if statement
    that declares the winner of the game based on the score of the player and computer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,5 @@
+function game() {
+
 // create an object that shows the winning combinations
 const winningCombinations = {
   rock: "scissors",
@@ -18,52 +20,64 @@ let gameCount = 0;
 let playerScore = 0;
 let computerScore = 0;
 
-while (gameCount < 5 && (playerScore < 3 && computerScore < 3) ) {
 
-  function getComputerChoice() {
-    let randomizedChoice = Math.floor(Math.random() * computerChoices.length)
 
-    return computerChoices[randomizedChoice];
+
+
+  while (gameCount < 5 && (playerScore < 3 && computerScore < 3) ) {
+
+    function getComputerChoice() {
+      let randomizedChoice = Math.floor(Math.random() * computerChoices.length)
+
+      return computerChoices[randomizedChoice];
+    }
+
+    const computerSelection = getComputerChoice();
+
+    console.log(computerSelection);
+
+    function gameSession(computerSelection, playerSelection) {
+      playerSelection = playerSelection.toLowerCase(); // Make player's selection case-insensitive
+
+      if (playerSelection === computerSelection) {
+        return `It's a tie! You both chose ${playerSelection}`
+
+      } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+      ){
+        playerScore++;
+
+            return  `You win! ${playerSelection} beats ${computerSelection}`
+
+    } else {
+      computerScore++;
+
+      return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+  }
+  // the players selection should be case insensitive
+  let playerSelection = prompt("Choose rock, paper, or scissors");
+  console.log(gameSession(computerSelection, playerSelection));
+
+  gameCount++;
+
+    if  (playerScore === 3) {
+      console.log ("You win best out of 3!")
+    } else if (computerScore === 3) {
+      console.log ("You lose best out of 3!")
+    } else if (gameCount === 5 && playerScore > computerScore) {
+      console.log (`You win ${playerScore} to ${computerScore}!`)
+    } else if (gameCount === 5 && computerScore > playerScore) {
+      console.log (`You lose ${computerScore} to ${playerScore}!`)
+    }
+
+    console.log(gameCount);
+    console.log(playerScore);
+    console.log(computerScore);
   }
 
-  const computerSelection = getComputerChoice();
-
-  console.log(computerSelection);
-
-  function gameSession(computerSelection, playerSelection) {
-    playerSelection = playerSelection.toLowerCase(); // Make player's selection case-insensitive
-
-    if (playerSelection === computerSelection) {
-      return `It's a tie! You both chose ${playerSelection}`
-
-    } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-    ){
-      playerScore++;
-
-          return  `You win! ${playerSelection} beats ${computerSelection}`
-
-  } else {
-    computerScore++;
-
-    return `You lose! ${computerSelection} beats ${playerSelection}`
-  }
 }
-// the players selection should be case insensitive
-let playerSelection = prompt("Choose rock, paper, or scissors");
-console.log(gameSession(computerSelection, playerSelection));
 
-gameCount++;
-
-  if  (playerScore === 3) {
-    console.log ("You win best out of 3!")
-  } else if (computerScore === 3) {
-    console.log ("You lose best out of 3!")
-  }
-
-  console.log(gameCount);
-  console.log(playerScore);
-  console.log(computerScore);
-}
+game();
