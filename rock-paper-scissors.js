94e9a982b711f38f3536cf355f05f7c9292261a0
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-23 22:51:22 +0100

    the logic behind game now works
    before the game would never end after 5 rounds, or at least it would'nt display the winner
    now it does both.. best out of three and if not whoever has the most points after 5 rounds wins.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,105 +1,97 @@
 function game() {
 
-// create an object that shows the winning combinations
-  const winningCombinations = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper",
-  }
-// create a variable that stores computer choices
-const computerChoices = Object.keys(winningCombinations);
-// create a variable that stores the player's score
-// create a variable that stores the computer's score
-// create a variable that stores the number of games played
-let gameCount = 0;
-let playerScore = 0;
-let computerScore = 0;
-let computerSelection = getComputerChoice();
-
-  // create variables that store the values of the buttons
-  // const rockButton = document.querySelector('#rock');
-  // const paperButton = document.querySelector('#paper');
-  // const scissorsButton = document.querySelector('#scissors');
-
-
+  // create an object that shows the winning combinations
+    const winningCombinations = {
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
+    }
+  // create a variable that stores computer choices
+  const computerChoices = Object.keys(winningCombinations);
+  // create a variable that stores the player's score
+  // create a variable that stores the computer's score
+  // create a variable that stores the number of games played
+  let gameCount = 0;
+  let playerScore = 0;
+  let computerScore = 0;
+  let computerSelection = getComputerChoice();
   let playerSelection = "";
+    // create variables that store the values of the buttons
+    // const rockButton = document.querySelector('#rock');
+    // const paperButton = document.querySelector('#paper');
+    // const scissorsButton = document.querySelector('#scissors');
 
+        function getComputerChoice() {
+          let randomizedChoice = Math.floor(Math.random() * computerChoices.length)
 
-
-
-  //while (gameCount < 5 && (playerScore < 3 && computerScore < 3) ) {
-
-      function getComputerChoice() {
-        let randomizedChoice = Math.floor(Math.random() * computerChoices.length)
-
-        return computerChoices[randomizedChoice];
-      }
-
-
-      console.log(`Computer chose: ${computerSelection}`);
-
-
-      const buttons = document.querySelectorAll('button');
-
-    buttons.forEach((button) => {
-      button.addEventListener('click', gameSession => {
-        playerSelection = button.id;
-        console.log(playerSelection);
-        // const computerSelection = getComputerChoice();
-        result = gameSession(computerSelection, playerSelection);
-        // console.log(result);
-        alert(result);
-        gameCount++;  // increment the game count
-
-          if (gameCount < 5) {
-            computerSelection = getComputerChoice();
-            console.log(`Computer chose: ${computerSelection}`);
-
-            if (gameCount === 5 && playerScore > computerScore) {
-              console.log(`You win ${playerScore} to ${computerScore}!`)
-            } else if (gameCount === 5 && computerScore > playerScore) {
-              console.log(`You lose ${computerScore} to ${playerScore}!`)
-
-            } if (gameCount < 5 && playerScore === 3) {
-
-              console.log("You win best out of 3!")
-            }else if (gameCount < 5 && computerScore === 3) {
-              console.log("You lose best out of 3!")
-            }
-          }
-
-
-      function gameSession(computerSelection, playerSelection) {
-          // playerSelection = playerSelection.toLowerCase();
-
-
-          if (playerSelection === computerSelection) {
-            return `It's a tie! You both chose ${playerSelection}`
-
-          } else if (
-          (playerSelection === "rock" && computerSelection === "scissors") ||
-          (playerSelection === "paper" && computerSelection === "rock") ||
-          (playerSelection === "scissors" && computerSelection === "paper")
-          ) {
-              playerScore++;
-
-                return  `You win! ${playerSelection} beats ${computerSelection}`
-
-            } else {
-          computerScore++;
-
-          return `You lose! ${computerSelection} beats ${playerSelection}`
+          return computerChoices[randomizedChoice];
         }
 
 
-      }
-        console.log(gameCount);
-        console.log(playerScore);
-        console.log(computerScore);
+        console.log(`Computer chose: ${computerSelection}`);
 
+
+        const buttons = document.querySelectorAll('button');
+
+      buttons.forEach((button) => {
+        button.addEventListener('click', gameSession => {
+          playerSelection = button.id;
+          console.log(playerSelection);
+          // const computerSelection = getComputerChoice();
+          result = gameSession(computerSelection, playerSelection);
+          // console.log(result);
+          alert(result);
+          gameCount++;  // increment the game count
+
+          computerSelection = getComputerChoice();
+
+            if (gameCount === 5 && playerScore > computerScore) {
+
+              console.log(`You win ${playerScore} to ${computerScore}!`)
+            } else if (gameCount === 5 && computerScore > playerScore) {
+              console.log(`You lose ${computerScore} to ${playerScore}!`)
+            } else {
+              if (gameCount < 5 && playerScore === 3) {
+
+                console.log("You win best out of 3!")
+            } else if (gameCount < 5 && computerScore === 3) {
+                console.log("You lose best out of 3!")
+            }
+          }
+
+            console.log(computerSelection)
+
+        function gameSession(computerSelection, playerSelection) {
+            // playerSelection = playerSelection.toLowerCase();
+
+
+
+            if (playerSelection === computerSelection) {
+              return `It's a tie! You both chose ${playerSelection}`
+
+            } else if (
+            (playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")
+            ) {
+                playerScore++;
+
+                  return  `You win! ${playerSelection} beats ${computerSelection}`
+
+              } else {
+            computerScore++;
+
+            return `You lose! ${computerSelection} beats ${playerSelection}`
+          }
+
+
+        }
+          console.log(gameCount);
+          console.log(playerScore);
+          console.log(computerScore);
+
+        })
       })
-    })
-  //}
-}
+  }
 
-game();
+  game();
